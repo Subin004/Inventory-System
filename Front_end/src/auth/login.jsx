a84a34747dff0c65b600a902b20a167df54f4c65
SARAVANA-KUMAR-G <kumargsaravanak@gmail.com>
2026-06-05 14:39:48 +0530

    Added login page

--- Front_end/src/auth/login.jsx
+++ Front_end/src/auth/login.jsx
@@ -2,8 +2,9 @@ function Login() {
 
     return (
         <>
-        <form action="" className="border-2 border-gray-300 p-4 rounded-lg">
-
+        <form action="">
+            
+            <h1>Hello, World!</h1>
 
         </form>
         </>
