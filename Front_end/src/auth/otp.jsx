de0eb3085573b8b9f00216ed0f7494bfb28b493d
SARAVANA-KUMAR-G <kumargsaravanak@gmail.com>
2026-06-07 14:50:35 +0530

    created otp.jsx page

---
+++ Front_end/src/auth/otp.jsx
@@ -0,0 +1,30 @@
+function Otp() {
+
+    return (
+        <>
+        <form action="">
+            
+            <h1 className="text-amber-300 text-6xl" >Hello, World!</h1>
+        
+    <div className="container items-center border-amber-500 p-4 rounded-md bg-gray-100 w-2xl "  >
+
+      <label>Enter your name:
+        <input type="text" className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+      </label>
+<br />
+      <label>Password:
+        <input type="password" className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+      </label>
+<br />
+      
+        <input type="button" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" value="Login" />
+
+      </div>
+
+        </form>
+        </>
+    );
+
+}
+
+export default Otp;
