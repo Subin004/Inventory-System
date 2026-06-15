d355e95b5ba617ad6cdc9e2576ffce5c4b7892cc
Subin004 <subinraja81275@gmail.com>
2026-06-15 12:17:57 +0530

    database connected

--- Back_end/server.js
+++ Back_end/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const dotenv = require("dotenv").config({ path: '.env'});
+const sequelize = require("./config/db");
+console.log("DB_PASSWORD =", process.env.DB_PASSWORD);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((err) => {
+    console.error("Database Connection Failed:", err);
+  });
 
 app.get("/", (req, res) => {
     res.send("Inventory Backend Running");
