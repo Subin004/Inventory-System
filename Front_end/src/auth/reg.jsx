64da053ba217e16e18be132f88fe6fb88fe60685
SARAVANA KUMAR G <kumargsaravanak@gmail.com>
2026-07-05 20:56:03 +0530

    Implement registration form with state management
    Refactor registration form to manage state and handle submissions.

--- Front_end/src/auth/reg.jsx
+++ Front_end/src/auth/reg.jsx
@@ -3,49 +3,91 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function Registration() {
-    return (
-        <>
-        <form action="">
-            <p>Please enter your credentials to access your account.</p>
-        
-    <div className="container items-center border-amber-500 p-4 rounded-md bg-gray-100 w-2xl "  >
+
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleNameChange(event){
+    setName(event.target.value);
+  }
+  function handleCompanyChange(event){
+    setCompany(event.target.value);
+  }
+  function handleEmailChange(event){
+    setEmail(event.target.value);
+  }
+  function handlePasswordChange(event){
+    setPassword(event.target.value);
+  }
+  function handleRegister(){
+    if(name && company && email && password){
+      const payload = {user_name: name, company_name: company, email: email, password: password};
+      axios.post("http://localhost:3000/api/register/", payload)
+      .then((response)=>{
+        console.log(response);
+        if(!response.ok){
+          throw Error("Registration Failed");
+        }
+        return response.data;
+      })
+      .then((data)=>{
+        console.log(data.msg);
+        console.log(data.data);
+        navigate('/login');
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    }
+    else{
+      console.log("Please full the fields!")
+    }
+  }
+  return (
+    <>
+    <form>
+      <p>Please enter your credentials to access your account.</p>
+      
+      <div className="container items-center border-amber-500 p-4 rounded-md bg-gray-100 w-2xl "  >
 
       <label>Enter your name:
-        <input type="text" className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        <input type="text" value={name} onChange={handleNameChange} className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
       </label>
-<br />
+      <br />
+
       <label>Company Name:
-        <input type="text" className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        <input type="text" value={company} onChange={handleCompanyChange} className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
       </label>
-<br />
+      <br />
+      
       <label>Email:
-        <input type="email" className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        <input type="email" value={email} onChange={handleEmailChange} className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
       </label>
-<br />  
+      <br />  
+      
       <label>Password:
-        <input type="password" className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        <input type="password" value={password} onChange={handlePasswordChange} className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
       </label>
-<br />
+      <br />
+    
       <input type="checkbox" id="remember" className="mr-2 leading-tight" />
       <label htmlFor="remember" className="text-sm text-gray-600">I agree to </label>
       <a href="/terms" className="text-sm text-blue-500 hover:underline">Terms and Conditions</a>
-<br />
-      
-        <input type="button" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" value="Register" />
+      <br />
+    
+      <input type="button" onClick={handleRegister} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" value="Register" />
 
-
-      
-      </div>
-
-  
-
-
-
-
-
+    </div>
 
   </form>
+  <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, cum facilis. Alias rerum minus cumque quasi amet odio ducimus aperiam ex. Distinctio iure expedita doloremque amet ea perspiciatis qui deserunt?</div>
   </>
   );
+
 }
+
 export default Registration;
